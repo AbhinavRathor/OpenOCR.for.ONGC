367ba0f9c4f0267aabd54860b270a794139d9e60
AbhinavRathor <abhinavrathor5888@gmail.com>
2025-08-31 12:34:40 +0530

    Remove virtual environments and secure API keys

--- backend/.venv/Lib/site-packages/beaker/utils.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-from google.protobuf.json_format import MessageToDict
-from google.protobuf.message import Message
-
-
-def pb2_to_dict(msg: Message) -> dict[str, Any]:
-    return MessageToDict(msg)
